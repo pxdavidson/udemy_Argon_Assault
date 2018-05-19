64f45abd2830b0357713bc05f69517833cd4ebe2
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-05-19 23:08:49 +0100

    AA_80A
    ##ASSETS
    + Flattened terrain to be more visually pleasing
    + Splash screen scene
    + Splash screen music & audio source
    
    ##CODE
    + Splash screen script
    + DismissSplash() waits until uyser input then loads first level

---
+++ Argon Assault/Assets/Splash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DismissSplash();
+    }
+
+    void DismissSplash()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
