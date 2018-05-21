5fd509e5b05cd045bd51f080144f85027f2929de
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-05-21 19:39:37 +0100

    AA_84A
    ##CODE
    + Player Script
    + Using UnityStandardAssets.CrossPlatformInputManager
    + float horizontalThrow

---
+++ Argon Assault/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    // Variables
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(horizontalThrow);
+	}
+}
