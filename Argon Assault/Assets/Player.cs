a596a4d65c91f9587d053c544d9ec58aeef465de
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-05-26 10:37:58 +0100

    AA_85A
    ## CODE
    + Speed var (set to 2f)
    + xOffset created
    + horizontalThrow renamed to xThrow
    
    xOffset now calculates movement required per frame based on CrossPlatformInput

--- Argon Assault/Assets/Player.cs
+++ Argon Assault/Assets/Player.cs
@@ -6,7 +6,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
     // Variables
-
+    [Tooltip("ms^-1")][SerializeField] float speed = 2f;
     
     // Use this for initialization
 	void Start ()
@@ -17,7 +17,8 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        print(xOffset);
 	}
 }
