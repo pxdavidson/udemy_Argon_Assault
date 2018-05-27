acb707105ec3aed2cd399f2aa5b927054842d6b4
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-05-27 20:36:58 +0100

    AA_87A
    ## CODE
    + MoveShipX() & MoveShipY()
    + Duplicated existing X axis functionality and adjusted for Y axis
    + Values set to Speed = 10f, ScreenX = 9f, ScreenY = 5f
    
    ## DATA
    + Adjusted ship position to be central when game starts

--- Argon Assault/Assets/Player.cs
+++ Argon Assault/Assets/Player.cs
@@ -7,11 +7,12 @@ public class Player : MonoBehaviour
 {
 
     // Variables
-    [Tooltip("ms^-1")][SerializeField] float Speed = 2f;
-    [SerializeField] float ScreenSize = 10;
-    
+    [Tooltip("ms^-1")][SerializeField] float Speed = 10f;
+    [SerializeField] float ScreenSizeX = 9;
+    [SerializeField] float ScreenSizeY = 5;
+
     // Use this for initialization
-	void Start ()
+    void Start ()
     {
 		
 	}
@@ -19,11 +20,29 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        MoveShipX();
+        MoveShipY();
+    }
+
+    // Move ship on X axis
+    private void MoveShipX()
+    {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * Speed * Time.deltaTime;
         float xRawPos = transform.localPosition.x + xOffset;
-        float xClampedPos = Mathf.Clamp(xRawPos, -ScreenSize, ScreenSize);
+        float xClampedPos = Mathf.Clamp(xRawPos, -ScreenSizeX, ScreenSizeX);
 
         transform.localPosition = new Vector3(xClampedPos, transform.localPosition.y, transform.localPosition.z);
-	}
+    }
+
+    // Move ship on Y axis
+    private void MoveShipY()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * Speed * Time.deltaTime;
+        float yRawPos = transform.localPosition.y + yOffset;
+        float yClampedPos = Mathf.Clamp(yRawPos, -ScreenSizeY, ScreenSizeY);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, yClampedPos, transform.localPosition.z);
+    }
 }
