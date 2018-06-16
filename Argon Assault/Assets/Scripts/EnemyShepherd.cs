014f2e6430eba344ac534f56ae9a8e1c5438bde0
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-06-16 18:07:40 +0100

    AA_100A
    # CODE
    + Add box colliders to enemy ships at runtime
    + Destroy ships when paretcile collision is detected.

---
+++ Argon Assault/Assets/Scripts/EnemyShepherd.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShepherd : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        CreateNonTriggerBoxCollider();
+    }
+
+    private void CreateNonTriggerBoxCollider()
+    {
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    // Detect particle collisions
+    void OnParticleCollision(GameObject otherGameObject)
+    {
+        Destroy(gameObject);
+    }
+}
