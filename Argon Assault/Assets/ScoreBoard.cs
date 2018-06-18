fc715da78a035f362be8c43e9080c415f45252fe
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-06-18 22:48:01 +0100

    AA_104A
    ## CODE
    + ScoreBoard class
    
    ## DATA
    + ka1 font for scoreboard
    + UI element with text

---
+++ Argon Assault/Assets/ScoreBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    // Variables
+    [SerializeField] int scorePerHit = 100;
+
+    int score;
+    Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit()
+    {
+        score = score + scorePerHit;
+        scoreText.text = score.ToString();
+    }
+
+}
